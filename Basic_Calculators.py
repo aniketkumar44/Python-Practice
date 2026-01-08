bb6d9a8b148218c9dc57ab273d7c61559207aeed
Aniket Kumar <aniketkumarptn151@gmail.com>
2026-01-08 22:44:05 +0530

    Add files via upload

---
+++ Basic_Calculators.py
@@ -0,0 +1,49 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("Sum =", sum)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+diff = a - b
+print("Difference =", diff)
+
+l = int(input("Enter length: "))
+b = int(input("Enter breadth: "))
+area = l * b
+print("Area =", area)
+
+r = int(input("Enter radius: "))
+cir = 2 * 3.14 * r
+print("Circumference =", cir)
+
+a = int(input("Enter base: "))
+b = int(input("Enter power: "))
+Pow = a ** b
+print("Power =", Pow)
+
+b = int(input("Enter base: "))
+h = int(input("Enter height: "))
+area = 0.5 * b * h
+print("Area =", area)
+
+p = int(input("Enter Principle: "))
+r = int(input("Enter Rate: "))
+t = int(input("Enter Time: "))
+si = (p * r * t) / 100
+print("Simple Interest =", si)
+
+a = int(input("Enter obtained marks: "))
+b = int(input("Enter total marks: "))
+per = (a / b) * 100
+print("Percentage =", per)
+
+d = int(input("Enter Distance: "))
+t = int(input("Enter Time: "))
+s = d / t
+print("Speed =", s)
+
+p = int(input("Enter Price: "))
+q = int(input("Enter Quantity: "))
+total = p * q
+print("Total Bill =", total)
---
+++ Simple_Shop_Bill.py
@@ -0,0 +1,21 @@
+print("ENTER OUR DETAILS")
+C=int(input("enter the number of cakes"))
+cp=C*30
+D=int(input("enter the number of cold drinks"))
+cd=D*25
+S=int(input("enter the number of snacks"))
+cs=S*20
+W=int(input("enter the number of waters"))
+cw=W*10
+TB=cp+cd+cs+cw
+print("***** BILL DETAILS *****")
+print("***** BIBS CANTEEN *****")
+print("************************")
+print("***** ITEM DETAILS *****")
+print("CAKE@30=",cp)
+print("COLD DRINK@25=",cd)
+print("SNACKS@20=",cs)
+print("WATER@10=",cw)
+print("TOTAL BILL=",TB)
+print("*****************")
+print("*****THANK YOU*****")
