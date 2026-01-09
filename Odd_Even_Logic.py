9e50a682a41b07e95b76a3956879ba7ba9f159bc
Aniket Kumar <aniketkumarptn151@gmail.com>
2026-01-09 13:08:56 +0530

    Update Odd_Even_Logic.py

--- Odd_Even_Logic.py
+++ Odd_Even_Logic.py
@@ -4,22 +4,4 @@ if(num%2==0):
     print("even")
 else:
     print("odd")
-    
-#check greatest number
-a=int(input("enter first number : "))
-b=int(input("enter second number : "))
-c=int(input("enter third number : "))
-if(a>=b and a>=c):
-    print("A",a)
-if(b>=c and b>=a):
-    print("b",b)
-if(c>=a and c>=b):
-    print("C",c)
-    
-#multiple of 5
 
-num=int(input("enter number : "))
-if(num%5==0):
-    print("multiple of 5")
-else:
-    print("not multiple")
