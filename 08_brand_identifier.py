d27c5d45fae1f53c917f2046f4ce9b8d449956d7
Aniket Kumar <aniketkumarptn151@gmail.com>
2026-01-11 14:04:05 +0530

    Add files via upload

---
+++ 08_brand_identifier.py
@@ -0,0 +1,14 @@
+ #brand_identifier
+brand=input("ENTER YOUR FAVOURITE BRAND NAME")
+if brand=="MRF":
+    print("IT IS A FAMOUS TYRE BRAND")
+elif brand=="TATA":
+    print("IT IS A FAMOUS STEEL BRAND")
+elif brand=="RAYMONDS":
+    print("IT IS A FAMOUS CLOTH BRAND")
+elif brand=="VOLTES":
+    print("IT IS A FAMOUS AC BRAND")
+elif brand=="HP":
+    print("IT IS A FAMOUS LAPTOP BRAND")
+else:
+    print("OTHER BRAND IS NOT RECOMENDED")
