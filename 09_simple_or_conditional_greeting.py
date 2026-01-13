2aeb178e2835e89c1918cb8ffa38e1209c71cbc8
Aniket Kumar <aniketkumarptn151@gmail.com>
2026-01-13 15:12:58 +0530

    Add files via upload

---
+++ 09_simple_or_conditional_greeting.py
@@ -0,0 +1,13 @@
+# simple_greeting
+name=input("ENTER YOUR NAME")
+print("HELLO",name,"GOOD MORNING")
+print("HOW ARE YOU")
+#conditional_greeting
+name=input("ENTER YOUR NAME")
+if name=="Aniket":
+    
+    print("HELLO ANIKET,GOOD MORNING")
+    print("HOW ARE YOU")
+else:
+    print("HELLO GUEST,GOOD MORNING")
+    print("HOW ARE YOU")
