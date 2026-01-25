b9c9352a2efcd309828c0c1aada5e4b216182c42
Aniket Kumar <aniketkumarptn151@gmail.com>
2026-01-25 11:21:22 +0530

    Add files via upload

---
+++ 21_While_Loop_Basics..py
@@ -0,0 +1,31 @@
+#print hello five times
+count = 1
+while count <= 5:
+     print("hello")
+     count += 1
+     print(count)
+#print world five times
+i=1
+while i<=5:
+    print("world",i)
+    i+=1
+print("-"*15)
+#print numbers from 1 to 10
+i = 1
+while i <= 10:
+     print(i)
+     i += 1
+print("-"*15)
+ #print numbers from 10 to 1
+i = 10
+while i >= 1:
+     print(i)
+     i -= 1
+print("-"*15)
+#User Confirmation Logic
+name=""
+print("*"*30)
+while(name!="Aniket"):
+    name=input(" ENTER USER ID : ")
+print("*"*30)
+print("***THANKS FOR CONFIRMATION***")
---
+++ 22_For_Loop_Range.py
@@ -0,0 +1,25 @@
+#odd number(0 to 20)
+for i in range(0,20):
+    if (i%2!=0):
+        print(i)
+print("-"*5)
+#even number(0 to 20)
+for i in range(0,20):
+    if (i%2==0):
+        print(i)
+print("-"*5)
+#using range
+for i in range(10): #range(stop)
+    print(i)
+print("-"*5)
+for i in range(2,10): #range(start,stop)
+    print(i)
+print("-"*5)
+# even number
+for i in range(2,101,2): #range(star,stop,step)
+    print(i)
+print("-"*5)
+#odd number
+for i in range(1,100,2):
+    print(i)
+
