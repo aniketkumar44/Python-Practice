e66f158ac5f4b8ecb8b046be9dfb01b814e3573c
Aniket Kumar <aniketkumarptn151@gmail.com>
2026-02-03 12:57:19 +0530

    Add files via upload

---
+++ 29_Menu_Calculator.py
@@ -0,0 +1,67 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+= RESTART: C:/Users/hp/AppData/Local/Programs/Python/Python314/28_Menu_Calculator.py
+--- SIMPLE CALCULATOR ---
+ MAIN MENU:
+1. Add Numbers (+)
+2. Subtract Numbers (-)
+3. Multiply Numbers (*)
+4.Divide Numbers (/)
+5. Exit Program
+------------------------------
+Enter your choice (1-5): 1
+------------------------------
+Enter 1st Number: 67
+Enter 2nd Number: 78
+Result:  145.0
+==============================
+ MAIN MENU:
+1. Add Numbers (+)
+2. Subtract Numbers (-)
+3. Multiply Numbers (*)
+4.Divide Numbers (/)
+5. Exit Program
+------------------------------
+Enter your choice (1-5): 2
+------------------------------
+Enter 1st Number: 67
+Enter 2nd Number: 98
+Result:  -31.0
+==============================
+ MAIN MENU:
+1. Add Numbers (+)
+2. Subtract Numbers (-)
+3. Multiply Numbers (*)
+4.Divide Numbers (/)
+5. Exit Program
+------------------------------
+Enter your choice (1-5): 3
+------------------------------
+Enter 1st Number: 67.85
+Enter 2nd Number: 3
+Result:  203.54999999999998
+==============================
+ MAIN MENU:
+1. Add Numbers (+)
+2. Subtract Numbers (-)
+3. Multiply Numbers (*)
+4.Divide Numbers (/)
+5. Exit Program
+------------------------------
+Enter your choice (1-5): 4
+------------------------------
+Enter 1st Number: 50
+Enter 2nd Number: 6
+Result:  8.333333333333334
+==============================
+ MAIN MENU:
+1. Add Numbers (+)
+2. Subtract Numbers (-)
+3. Multiply Numbers (*)
+4.Divide Numbers (/)
+5. Exit Program
+------------------------------
+Enter your choice (1-5): 5
+------------------------------
+Exiting... Have a nice day! 👋
